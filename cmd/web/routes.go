52a837ba16f863b6e0b63dbde54eb6fe18a9396e
Sacrament0 <91359171+Sacrament0@users.noreply.github.com>
2022-02-13 17:15:47 +0300

    images added

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,12 +14,18 @@ func routes(app *config.AppConfig) http.Handler {
 	// создаем мультиплексор
 	mux := chi.NewRouter()
 
+	// middleware
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
+	// хэндлеры
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	// обработка статических файлов
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
